4bcdef4be25c3c645e266d12f5b71fe7b34b3ab4
Josh Pack <josh.packGmail.com>
2022-06-27 23:47:18 -0700

    working, alpha

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -92,7 +92,8 @@ const NavBar = () => {
               className="flex justify-between items-center w-full text-gray-300"
               href="/"
             >
-              Linked In <FaLinkedinIn size={30} />
+              LinkedIn
+              <FaLinkedinIn size={30} />
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
